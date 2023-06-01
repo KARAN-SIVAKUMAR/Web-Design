70540426d41ddcdbf248d2b3c845dc60960fa4c7
Karan Sivakumar <130133928+KARAN-SIVAKUMAR@users.noreply.github.com>
2023-06-01 10:57:46 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,9 @@
+function toggleSidebar(){
+    document.getElementsByClassName('sidebar');
+    sidebar.style.display == "block";
+    if (sidebar.style.display == "block"){ sidebar.style.display = "none";
+ 	}
+else {
+sidebar.style.display = "block";
+ 	}
+}
